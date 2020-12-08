72e637faf39b6809adc7dd8469ca5c208ba5a882
EquipoAzulalmacenamiento <73922550+EquipoAzulalmacenamiento@users.noreply.github.com>
2020-12-08 11:39:18 -0600

    Update product.js

--- models/product.js
+++ models/product.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const ProductShema=Schema({
-    entrada: String 
+    estado: String,
+    temperatura: Number,
+    fecha:Date
 })
 
 module.exports = mongoose.model('Product', ProductShema)
